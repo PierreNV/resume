f9be769c08807d665f3746e459a5320724cc68ac
PierreNV <pierre_n@live.fr>
2022-10-27 12:25:27 +0200

    Commit 12:30

--- Resume.js
+++ Resume.js
@@ -1,16 +1,10 @@
 document.getElementById("container_dropdown_button").onclick = function () {
-  openMenuFunction();
-};
-function openMenuFunction() {
   document
     .getElementById("container_menu_wrapper")
     .classList.toggle("menu_toggle");
   document.getElementById("wrapper").classList.toggle("wrapper_toggle");
-}
+};
 
 document.getElementById("print-button").onclick = function () {
-  printFunction();
-};
-function printFunction() {
   window.print();
-}
+};
