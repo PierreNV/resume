09325cc2ca52278550e951b609c159c40019d635
PierreNV <pierre_n@live.fr>
2022-10-31 11:22:55 +0100

    commit 11.20

--- resume.js
+++ resume.js
@@ -1,9 +1,13 @@
 document.getElementById("container_dropdown_button").onclick = function () {
+  menuToggleFunction();
+};
+
+function menuToggleFunction() {
   document
     .getElementById("container_menu_wrapper")
     .classList.toggle("menu_toggle");
   document.getElementById("wrapper").classList.toggle("wrapper_toggle");
-};
+}
 
 document.getElementById("print-button").onclick = function () {
   window.print();
