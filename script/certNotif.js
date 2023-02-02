2a9874201dcba95667538823b7fe814fc060f208
PierreNV <pierre_n@live.fr>
2023-02-02 19:02:20 +0100

    update

--- script/certNotif.js
+++ script/certNotif.js
@@ -1,20 +1,20 @@
 "use strict";
 
 document.getElementById("cert-checkbox").onclick = function () {
-  const userMessage = document.getElementById("message");
-  const certValue = document.getElementById("cert-value");
-  function certYes() {
-    certValue.value = "yes";
-    userMessage.value = "*Request Certificates.*" + "\n" + userMessage.value;
-  }
-  function certNo() {
-    certValue.value = "no";
-    userMessage.value = userMessage.value.replace(
-      /[*]Request Certificates.[*]/gi,
-      ""
-    );
-  }
+	const userMessage = document.getElementById("message");
+	const certValue = document.getElementById("cert-value");
+	function certYes() {
+		certValue.value = "yes";
+		userMessage.value = "*Request Certificates.*" + "\n" + userMessage.value;
+	}
+	function certNo() {
+		certValue.value = "no";
+		userMessage.value = userMessage.value.replace(
+			/[(]Request Certificates.[)]/gi,
+			""
+		);
+	}
 
-  if (document.getElementById("cert-checkbox").checked) certYes();
-  else certNo();
+	if (document.getElementById("cert-checkbox").checked) certYes();
+	else certNo();
 };
