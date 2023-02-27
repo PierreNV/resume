c5df5bf861054430afeecc0797c8b421d893d752
PierreNV <pierre_n@live.fr>
2023-02-27 21:30:10 +0100

    update scroll mobile

--- script/languageSwitcher.js
+++ script/languageSwitcher.js
@@ -4,13 +4,13 @@ function languageSwitcher() {
     if (!window.localStorage.getItem('language')) {
         window.localStorage.setItem('language', 'sv');
         window.location.hash = "sv";
-        window.scroll({top: 0, behavior: "smooth"});
+        window.scrollTo(0, 0);
         window.location.reload(true);
         return;
     } 
     else {
         window.localStorage.clear("language");
         window.location.hash = "";
-        window.scroll({top: 0, behavior: "smooth"});
+        window.scrollTo(0, 0);
         window.location.reload(true);
 };};
