f602e27bf2585a1b5fbd7df87f71718f14ad0075
PierreNV <pierre_n@live.fr>
2023-02-27 19:05:44 +0100

    animation update

--- script/languageSwitcher.js
+++ script/languageSwitcher.js
@@ -4,13 +4,13 @@ function languageSwitcher() {
     if (!window.localStorage.getItem('language')) {
         window.localStorage.setItem('language', 'sv');
         window.location.hash = "sv";
-        window.location.reload();
-        window.scroll({top: 0, behavior: "smooth"});
+        window.location.reload(true);
+        document.scroll({top: 0, behavior: "smooth"});
         return;
     } 
     else {
         window.localStorage.clear("language");
         window.location.hash = "";
-        window.location.reload();
-        window.scroll({top: 0, behavior: "smooth"});
+        window.location.reload(true);
+        document.scroll({top: 0, behavior: "smooth"});
 };};
