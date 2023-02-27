ba08f6ed963c85aa1c8282f59451d0c7ca82c857
PierreNV <pierre_n@live.fr>
2023-02-27 11:30:10 +0100

    update

--- script/navBarToggler.js
+++ script/navBarToggler.js
@@ -5,9 +5,3 @@ function navBarToggle() {
     toggleTarget[i].classList.toggle("display-none");
   }
 };
-// document.getElementById("dropdown-button").onclick = function navBarToggler() {
-//   const toggleTarget = document.getElementsByClassName("toggle-target");
-//   for (let i = 0; i < toggleTarget.length; i++) {
-//     toggleTarget[i].classList.toggle("display-none");
-//   }
-// };
