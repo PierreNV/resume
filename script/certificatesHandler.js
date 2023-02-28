16e370c12d24eb96e154030959747c3485ac9394
PierreNV <pierre_n@live.fr>
2023-02-28 15:04:23 +0100

    certificatesHandler clean up

--- script/certificatesHandler.js
+++ script/certificatesHandler.js
@@ -2,9 +2,7 @@
 
 function certificatesNotification() {
   const request = document.getElementById("cert-request");
-  if (document.getElementById("cert-checkbox").checked) {
-    request.value = "Certificates are requested...";
-  } else {
-    request.value = "";
-  }
+  document.getElementById("cert-checkbox").checked
+    ? (request.value = "Certificates are requested...")
+    : (request.value = "");
 }
