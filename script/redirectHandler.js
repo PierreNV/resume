7031e582f9e001dc2d5bf6cd42df04a61ff375ed
PierreNV <pierre_n@live.fr>
2023-03-12 11:59:56 +0100

    redirectHandler

--- script/redirectHandler.js
+++ script/redirectHandler.js
@@ -1,7 +1,8 @@
 "use strict";
 
-if (window.location.pathname.toString().includes("pierrenv.github.io")) {
-  console.log(window.location.pathname.toString());
+if (
+  window.location.pathname === "https://pierrenv.github.io/resume/index.html"
+) {
   window.location.replace("https://pierre-neveu-resume.netlify.app/");
   window.location.reload(true);
 }
