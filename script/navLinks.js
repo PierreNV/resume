19a1465d68d5582dfde5e5d7191d9d12f40b1517
PierreNV <pierre_n@live.fr>
2023-03-26 18:08:57 +0200

    href building fix

--- script/navLinks.js
+++ script/navLinks.js
@@ -1,20 +1,17 @@
-// const links = ["Home", "Resume", "Contact"];
-
 export const createLinks = (links) => {
   const menu = document.querySelector(".menu");
 
   links.forEach((link) => {
     const navLink = document.createElement("a");
+
     navLink.id = `menu__link__${link.toLowerCase()}`;
     navLink.classList = "menu__links menu__links__bg buttons-effect";
     navLink.textContent = `${link}`;
     link === "Home"
       ? (navLink.href = "index.html")
-      : (navLink.href = `${link.toLowerCase()}.html`);
+      : (navLink.href = `${link.toLowerCase()}` + ".html");
     navLink.setAttribute("data-aos", "flip-down");
     navLink.setAttribute("data-aos-duration", "800");
     menu.append(navLink);
   });
 };
-
-// createLinks(links);
