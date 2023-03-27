b5d8f4e7c3e9732fd9ade786087622fe4e17ade7
PierreNV <pierre_n@live.fr>
2023-03-27 09:59:30 +0200

    commit

--- script/navLinks.js
+++ script/navLinks.js
@@ -7,9 +7,10 @@ export const createLinks = (links) => {
     navLink.id = `menu__link__${link.toLowerCase()}`;
     navLink.classList = "menu__links menu__links__bg buttons-effect";
     navLink.textContent = `${link}`;
-    link === "Home"
-      ? (navLink.href = "index.html")
-      : (navLink.href = `${link.toLowerCase()}.html`);
+    // link === "Home"
+    //   ? (navLink.href = "index.html")
+    //   : (navLink.href = `${link.toLowerCase()}.html`);
+    navLink.href = "index.html";
     navLink.setAttribute("data-aos", "flip-down");
     navLink.setAttribute("data-aos-duration", "800");
     menu.append(navLink);
