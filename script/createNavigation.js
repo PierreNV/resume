4af461b8b561ab6b6c796d9e3c218857a2eb5fbd
PierreNV <pierre_n@live.fr>
2024-03-11 14:39:22 +0100

    commit

--- script/createNavigation.js
+++ script/createNavigation.js
@@ -5,7 +5,7 @@ import { createDropDownButton } from "./navButton.js";
 import { createLinks } from "./navLinks.js";
 import { createFooter } from "./navFooter.js";
 
-const pages = ["Home", "Projects", "Game", "Contact"];
+const pages = ["Home", "Resume", "Game", "Contact"];
 
 const createNavigation = (links) => {
   createDropDownButton();
